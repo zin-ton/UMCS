29c22b7322ba77f937fb677540d9e5cca80eedb0
zin-ton <75830572+zin-ton@users.noreply.github.com>
2022-10-12 19:03:03 +0200

    12.10.2022 UMCS Podstawy Programowania

---
+++ krótki_zapis.cpp
@@ -0,0 +1,19 @@
+if(warunek){
+    blok A
+    result = 1;
+}
+else{
+    blok B
+    result = 2;
+}
+==
+result = warunek ? blok A : blok B;
+
+if(warunek){
+    blok A
+}
+else{
+    blok B
+}
+==
+warunek ? blok A : blok B;
